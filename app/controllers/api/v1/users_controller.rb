a12caef3527fbb72c8860d58ab6417487d3d44c7
caioagiani <agianicaio@gmail.com>
2020-08-27 18:12:45 -0300

    feat: added create action on users controller with tests

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -11,4 +11,21 @@ class Api::V1::UsersController < ApplicationController
     end
   end
 
+  def create
+    user =  User.new(user_params)
+
+    if user.save
+      render json: user, status: 201
+    else
+      render json: { errors: user.errors }, status: 422
+    end
+
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
+
 end
--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -3,12 +3,14 @@ require 'rails_helper'
 RSpec.describe 'Users API', type: :request do
   let!(:user) { create(:user) }
   let(:user_id) { user.id }
+  let(:headers) do
+    { 'Accept' => 'application/vnd.taskmanager.v1' }
+  end
 
   before { host! 'api.task-manager.test' }
 
   describe 'GET /api/users/:id' do
     before do
-      headers = { 'Accept' => 'application/vnd.taskmanager.v1' }
       get "/api/users/#{user_id}", params: {}, headers: headers
     end
 
@@ -32,4 +34,39 @@ RSpec.describe 'Users API', type: :request do
       end
     end
   end
+
+  describe 'POST /api/users' do
+    before do
+      post '/api/users', params: { user: user_params }, headers: headers
+    end
+
+    context 'when the request params are valid' do
+      let(:user_params) { attributes_for(:user) }
+
+      it 'returns status code 201' do
+        expect(response).to have_http_status(201)
+      end
+
+      it 'returns json data for created user' do
+        user_response = JSON.parse(response.body)
+        expect(user_response['email']).to eq(user_params[:email])
+      end
+
+    end
+
+    context 'when the request params are invalid' do
+      let(:user_params) { attributes_for(:user, email: 'invalid_mail') }
+
+      it 'returns status code 422' do
+        expect(response).to have_http_status(422)
+      end
+
+      it 'returns json data for erros' do
+        user_response = JSON.parse(response.body)
+        expect(user_response).to have_key('errors')
+      end
+
+    end
+  end
+
 end
