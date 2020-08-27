dfd4497561c6c2a6ab161987e89deb1e52198d3b
caioagiani <agianicaio@gmail.com>
2020-08-27 13:03:22 -0300

    feat: added shoulda matchers for user specs

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  attr_accessor :name
+  
+  validates_presence_of :name  
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,11 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  before { @user = FactoryGirl.build(:user) }
+  let(:user) { build(:user) }
 
-  it { expect(@user).to respond_to(:email) }
-  it { expect(@user).to respond_to(:name) }
-  it { expect(@user).to respond_to(:password) }
-  it { expect(@user).to respond_to(:password_confirmation) }
-  it { expect(@user).to be_valid }
+  it { is_expected.to validate_presence_of(:email) }
+  it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
+  it { is_expected.to allow_value("caio.agiani@gmail.com").for(:email) } 
+  it { is_expected.to validate_confirmation_of(:password) }
+
+
+  # context 'when name is blank' do
+  #   before { user.name = " " }
+    
+  #   it { expect(user).not_to be_valid } 
+  # end
+
+  # context 'when name is nil' do
+  #   before { user.name = nil }
+    
+  #   it { expect(user).not_to be_valid } 
+  # end
+
+  # subject { build(:user) }
+  # before { @user = FactoryGirl.build(:user) }
+
+  # it { expect(@user).to respond_to(:email) }
+
+  # it { expect(subject).to respond_to(:email) }
+
+  # it { is_expected.to respond_to(:email) }
+  # it { is_expected.to respond_to(:name) }
+  # it { is_expected.to respond_to(:password) }
+  # it { is_expected.to respond_to(:password_confirmation) }
+  # it { is_expected.to be_valid }
 end
