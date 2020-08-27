57bc8b195f32aa38f5176b32558457cc37659870
caioagiani <agianicaio@gmail.com>
2020-08-26 21:10:22 -0300

    feat: added mime_type application v1 default

--- README.md
+++ README.md
@@ -5,8 +5,10 @@ application up and running.
 
 Commands to download dependencies and start the application:
 
-1. `bundle install`
-2. `sudo -- sh -c -e "echo '127.0.0.1 api.task-manager.test' >> /etc/hosts";`
+1. `sudo -- sh -c -e "echo '127.0.0.1 api.task-manager.test' >> /etc/hosts";`
+   1.1. `bundle install`
+   1.2. `rails db:create`
+   1.3. `rails s`
 
 Commands used to generate the application:
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
+require 'api_version_constraint'
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, patch: '/'  do
+
+    namespace :v1, path: '/', constraints: ApiVersionConstraint.new(version: 1, default: true) do
+    end
 
   end
 end
