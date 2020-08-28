7a698a77848a77b4ca627df85233b51b5bc2ce6f
caioagiani <agianicaio@gmail.com>
2020-08-27 21:31:16 -0300

    feat: fix crud

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -12,14 +12,13 @@ class Api::V1::UsersController < ApplicationController
   end
 
   def create
-    user =  User.new(user_params)
+    user = User.new(user_params)
 
     if user.save
       render json: user, status: 201
     else
       render json: { errors: user.errors }, status: 422
     end
-
   end
 
   def update
@@ -37,7 +36,6 @@ class Api::V1::UsersController < ApplicationController
 
     head 204
   end
-
 
   private
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, and :omniauthable
+  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  attr_accessor :name
+  # attr_accessor :name
 
   # validates_presence_of :name
 end
